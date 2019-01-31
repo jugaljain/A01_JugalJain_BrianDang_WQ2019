302bb4e75a0c6b541db94e1aef7d13613a63a52a
briandang77 <briandang77@gmail.com>
2019-01-30 21:13:17 -0800

    added names and checkoff sheet

--- lab2redux/accel2/main.c
+++ lab2redux/accel2/main.c
@@ -1,48 +1,9 @@
 //*****************************************************************************
-//
-// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
-// 
-// 
-//  Redistribution and use in source and binary forms, with or without 
-//  modification, are permitted provided that the following conditions 
-//  are met:
-//
-//    Redistributions of source code must retain the above copyright 
-//    notice, this list of conditions and the following disclaimer.
-//
-//    Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the 
-//    documentation and/or other materials provided with the   
-//    distribution.
-//
-//    Neither the name of Texas Instruments Incorporated nor the names of
-//    its contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//*****************************************************************************
-
+// Students:Brian Dang, Jugal Jain
 //*****************************************************************************
 //
-// Application Name     - SPI Demo
-// Application Overview - The demo application focuses on showing the required 
-//                        initialization sequence to enable the CC3200 SPI 
-//                        module in full duplex 4-wire master and slave mode(s).
-// Application Details  -
-// http://processors.wiki.ti.com/index.php/CC32xx_SPI_Demo
-// or
-// docs\examples\CC32xx_SPI_Demo.pdf
+// Application Name     - Acceleramator
+// Application Overview - Reads acceleramotor data for X and Y and moves a circle arround the OLED display
 //
 //*****************************************************************************
 
@@ -340,7 +301,7 @@ void main()
                 y = 4;
             }
 
-            //draw circle in x and y value, switched because axis was flipped from what I orignally planned
+            //draw circle in x and y value, switched because axis was flipped from what I originally planned
             fillCircle((int) y, (int) x, 4, BLUE);
 
         }
