f957b0aefc33e25a156ed5082b10c2699be63b62
briandang77 <briandang77@gmail.com>
2019-03-07 15:55:44 -0800

    added comments

--- lab5/SSL_REST_API_AWS_W19/main.c
+++ lab5/SSL_REST_API_AWS_W19/main.c
@@ -1,47 +1,7 @@
 //*****************************************************************************
-//
-// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
-// 
-// 
-//  Redistribution and use in source and binary forms, with or without 
-//  modification, are permitted provided that the following conditions 
-//  are met:
-//
-//    Redistributions of source code must retain the above copyright 
-//    notice, this list of conditions and the following disclaimer.
-//
-//    Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the 
-//    documentation and/or other materials provided with the   
-//    distribution. 
-//
-//    Neither the name of Texas Instruments Incorporated nor the names of
-//    its contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-//
-//*****************************************************************************
-
-
-//*****************************************************************************
-//
-// Application Name     -   SSL Demo
-// Application Overview -   This is a sample application demonstrating the
-//                          use of secure sockets on a CC3200 device.The
-//                          application connects to an AP and
-//                          tries to establish a secure connection to the
-//                          Google server.
+// Authors              -   Brian Dang, Jugal Jain
+// Application Name     -   AWS Demo
+// Application Overview -   Connects to AWS and tests POST and GET in REST
 // Application Details  -
 // docs\examples\CC32xx_SSL_Demo_Application.pdf
 // or
@@ -901,6 +861,7 @@ void main() {
     if(lRetVal < 0) {
         ERR_PRINT(lRetVal);
     }
+    //post message and then immediately get the message
     http_post(lRetVal);
     http_get(lRetVal);
 
@@ -986,6 +947,7 @@ static int http_get(int iTLSSockID){
     int lRetVal = 0;
 
     pcBufHeaders = acSendBuff;
+    //slightly change the headers used in POST to instead do GET
     strcpy(pcBufHeaders, GETHEADER);
     pcBufHeaders += strlen(GETHEADER);
     strcpy(pcBufHeaders, HOSTHEADER);
--- lab5/amazon_texting/main.c
+++ lab5/amazon_texting/main.c
@@ -1,47 +1,8 @@
 //*****************************************************************************
 //
-// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
-// 
-// 
-//  Redistribution and use in source and binary forms, with or without 
-//  modification, are permitted provided that the following conditions 
-//  are met:
-//
-//    Redistributions of source code must retain the above copyright 
-//    notice, this list of conditions and the following disclaimer.
-//
-//    Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the 
-//    documentation and/or other materials provided with the   
-//    distribution. 
-//
-//    Neither the name of Texas Instruments Incorporated nor the names of
-//    its contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-//
-//*****************************************************************************
-
-
-//*****************************************************************************
-//
-// Application Name     -   SSL Demo
-// Application Overview -   This is a sample application demonstrating the
-//                          use of secure sockets on a CC3200 device.The
-//                          application connects to an AP and
-//                          tries to establish a secure connection to the
-//                          Google server.
+// Authors              -   Brian Dang, Jugal Jain
+// Application Name     -   AWS Texting
+// Application Overview -   Connects to an AWS Server and sends texts to a phone
 // Application Details  -
 // docs\examples\CC32xx_SSL_Demo_Application.pdf
 // or
@@ -1282,10 +1243,11 @@ void main() {
                 Outstr(message);
                 setCursor(0, 0);
             }
-            //else put into UART
+            //else POST to AWS Server
             else if (button == 11 && (strlen(message)) >= 0)
             {
                 int i = 0;
+                //disable timer so POST isn't interrupted
                 TimerDisable(TIMERA2_BASE, TIMER_A);
                 http_post(lRetVal, message);
                 TimerEnable(TIMERA2_BASE, TIMER_A);
@@ -1325,6 +1287,7 @@ static int http_post(int iTLSSockID, char* message){
     pcBufHeaders += strlen(CHEADER);
     strcpy(pcBufHeaders, "\r\n\r\n");
 
+    //edited DATA1 to cut off string for var and instead post the texting message
     int dataLength = strlen(DATA1) + strlen(message) + strlen( "\"\r\n}}}\r\n\r\n");
 
     strcpy(pcBufHeaders, CTHEADER);
@@ -1386,6 +1349,7 @@ static int http_get(int iTLSSockID){
     char* pcBufHeaders;
     int lRetVal = 0;
 
+    //slightly change the headers used in POST to instead do GET
     pcBufHeaders = acSendBuff;
     strcpy(pcBufHeaders, GETHEADER);
     pcBufHeaders += strlen(GETHEADER);
